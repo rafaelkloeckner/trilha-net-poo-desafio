0e4cdc8680fcf16453330bbd7d47513c321b47da
rafaelkloeckner <rafaelkloeckner@gmail.com>
2022-09-27 13:20:04 -0400

    Implementação das heranças e métodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "198765", modelo: "ModeloN1", imei: "ARCB*#NF(#*%84293", memoria: 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "34862618", modelo: "ModeloIPH1", imei: "FRGT423*#NF423(#*%84293", memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facetime");
